275fe0e4b7624eb5a196e0de9c5351e973ec3633
denrzv <voofaa@gmail.com>
2022-08-11 07:25:08 +0300

    hw done

---
+++ src/Client.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+    private final String host;
+    private final int port;
+    private final long START_DELAY = 1000;
+
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() {
+        try (Socket socket = new Socket(host, port);
+
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+             Scanner scanner = new Scanner(System.in)) {
+
+            Thread.sleep(START_DELAY);
+            System.out.println("Введите номер N-го члена ряда Фибоначчи для подсчёта. Для выхода наберите 'end'.");
+            String msg;
+
+            while (scanner.hasNext()) {
+                msg = scanner.nextLine();
+                out.println(msg);
+                if (msg.equals("end")) {
+                    System.out.println("Клиент завершает работу");
+                    break;
+                }
+                System.out.println(in.readLine());
+            }
+
+        } catch (UnknownHostException e) {
+            System.err.println("Неверно задан адрес сервера!");
+        } catch (IOException e) {
+            System.err.println("Ошибка в работе клиента!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends Thread {
+    private final int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    private int getNthFibonacci(int n) {
+        int a = 0, b = 1, c;
+        if (n == 0)
+            return a;
+        for (int i = 2; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+
+    @Override
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket socket = serverSocket.accept();
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            System.out.println("Сервер запущен " + serverSocket.getInetAddress().getHostAddress() + ":" +
+                    serverSocket.getLocalPort());
+            System.out.println("Новое подключение от клиента " + socket.getInetAddress().getHostAddress());
+
+            String line;
+            while ((line = in.readLine()) != null && !Thread.interrupted()) {
+                if (line.equals("end")) {
+                    throw new InterruptedException();
+                } else if (line.chars().allMatch(Character::isDigit)) {
+                    System.out.println("Идёт подсчёт данных...");
+                    int nth = getNthFibonacci(Integer.parseInt(line));
+                    out.println("Результат вычислений -> " + nth);
+                } else {
+                    out.println("Введены некорректные данные для подсчёта");
+                }
+            }
+        } catch (IOException e) {
+                System.err.println("Ошибка в работе сервера");
+        } catch (InterruptedException e) {
+                System.out.println("Сервер завершает работу");
+                Thread.currentThread().interrupt();
+        }
+    }
+}
